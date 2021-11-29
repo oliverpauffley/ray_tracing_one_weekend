356a2ae65ec6f8cda3fee24131920659f3585856
Ollie <mrpauffley@gmail.com>
2021-11-29 08:37:03 +0000

    add hittable spheres shaded through a normal map

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::{mem::discriminant, ops::Mul};
+
 use crate::vec::{Color, Point3, Vec3};
 use ray::Ray;
 
@@ -9,7 +11,26 @@ const ASPECT_RATIO: f64 = 16.0 / 9.0;
 const IMAGE_WIDTH: usize = 400;
 const IMAGE_HEIGHT: usize = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as usize;
 
+fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> f64 {
+    let oc = r.origin - *center;
+    let a = r.direction.dot(&r.direction);
+    let b = 2.0 * oc.dot(&r.direction);
+    let c = oc.dot(&oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+    if discriminant.is_sign_negative() {
+        -1.0
+    } else {
+        (-b - discriminant.sqrt()) / 2.0 * a
+    }
+}
+
 fn ray_color(r: &Ray) -> Color {
+    let t = hit_sphere(&Point3::new(0.0, 0.0, -1.0), 0.5, r);
+    if t > 0.0 {
+        let n = (r.at(t) - Vec3::new(0.0, 0.0, -1.0)).unit_vector();
+        return Color::new(n.0 + 1.0, n.1 + 1.0, n.2 + 1.0).mul_scalar(0.5);
+    }
+    // produce a blended blue background in the y direction.
     let unit_direction = r.direction.unit_vector();
     let t = 0.5 * (unit_direction.1 + 1.0);
     Color::new(1.0, 1.0, 1.0).mul_scalar(1.0 - t) + Color::new(0.5, 0.7, 1.0).mul_scalar(t)
